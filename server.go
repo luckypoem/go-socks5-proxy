b1ebd615054c288f8a6efb60eb13dfc1ed95cf8a
briteming <briteming@gmail.com>
2018-08-24 16:46:04 +0800

    Update server.go

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ func main() {
 	}
 
 	// Create SOCKS5 proxy on localhost port 1080
-	if err := server.ListenAndServe("tcp", "0.0.0.0:1080"); err != nil {
+	if err := server.ListenAndServe("tcp", "0.0.0.0:30801"); err != nil {
 		panic(err)
 	}
 }
